c8521136ab032bc71c7813fc29b4041a0293b99c
Stanislav <99604257+staszarzarstas@users.noreply.github.com>
2024-10-01 04:12:06 +0300

    Add files via upload

---
+++ SneakPeek/scripts/main.js
@@ -0,0 +1,17 @@
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    mousewheel: true,
+    keyboard: true,
+  });
+
+
+
+
+  
